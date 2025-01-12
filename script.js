4f9bda1abe83c9f3aeec363961d4f35cc62deb12
HasiniUndapalli <121223282+hasini3@users.noreply.github.com>
2025-01-12 19:39:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown-content');
+    dropdown.classList.toggle('active');
+  }
+  function toggledropdown() {
+    const dropdown = document.querySelector('.more-content');
+    dropdown.classList.toggle('active');
+  }
+  
